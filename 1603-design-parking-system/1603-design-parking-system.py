75a63ab32f784098ee3381c3e85e9e05976bf81c
Bhupender Ram Niwas Ojha <b.ram.niwas.ojha@gmail.com>
2022-05-11 11:35:54 +0530

    Time: 188 ms (42.84%), Space: 14.5 MB (64.92%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -10,15 +10,17 @@ class ParkingSystem:
             if self.big>0:
                 self.big-=1
                 return True
+            return False
         if carType==2:
             if self.medium>0:
                 self.medium-=1
                 return True
+            return False
         if carType==3:
             if self.small>0:
                 self.small-=1
                 return True
-        return False
+            return False
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
