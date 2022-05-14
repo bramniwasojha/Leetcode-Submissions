69c22bb56efe6e0cdd7042785ce3a2b53a982de7
Bhupender Ram Niwas Ojha <b.ram.niwas.ojha@gmail.com>
2022-05-15 03:38:50 +0530

    Time: 350 ms (41.68%), Space: 16 MB (54.19%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.py
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.py
@@ -1,19 +1,28 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        c=0
-        hashmap={}
-        for i in range(len(words)):
-            temp=0
-            for j in range(len(words[i])):
-                if hashmap.get(words[i][j],0)!=0:
-                    continue
-                else:
-                    if words[i][j] in allowed:
-                        hashmap[words[i][j]]=1
-                    else:
-                        temp=1
-                        break
-            if temp==0:
-                c+=1
+        res=len(words)
+        for w in words:
+            for l in w:
+                if l not in allowed:
+                    res-=1
+                    break
+        return res
+        
+        
+#         c=0
+#         hashmap={}
+#         for i in range(len(words)):
+#             temp=0
+#             for j in range(len(words[i])):
+#                 if hashmap.get(words[i][j],0)!=0:
+#                     continue
+#                 else:
+#                     if words[i][j] in allowed:
+#                         hashmap[words[i][j]]=1
+#                     else:
+#                         temp=1
+#                         break
+#             if temp==0:
+#                 c+=1
                         
-        return c
+#         return c
