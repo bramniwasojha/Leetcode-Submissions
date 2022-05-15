134de567c602330a2986479d7e04bf3656d49123
Bhupender Ram Niwas Ojha <b.ram.niwas.ojha@gmail.com>
2022-05-15 14:50:20 +0530

    Time: 69 ms (58.15%), Space: 13.8 MB (85.11%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,9 +1,7 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        hash1={}
-        for city in paths:
-            hash1[city[0]]=1
-        for city in paths:
-            if hash1.get(city[1],0)==0:
-                return city[1]
-        return
+        l=[path[1] for path in paths]
+        for path in paths:
+            if path[0] in l:
+                l.remove(path[0])
+        return l[0]
