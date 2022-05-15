8051cb22617cb8932efe92e5cc81002ae6160766
Bhupender Ram Niwas Ojha <b.ram.niwas.ojha@gmail.com>
2022-05-15 14:10:48 +0530

    Time: 62 ms (22.44%), Space: 13.9 MB (34.38%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -4,9 +4,9 @@ class Solution:
         b=0
         for i in range(len(s)):
             if i<len(s)//2:
-                if s[i] in  {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}:
+                if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U':
                     a+=1
             else:
-                if s[i] in  {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}:
+                if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U':
                     b+=1
         return a==b
