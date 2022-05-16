8032b5f775494c213acbc30eef226382c87e2a89
Bhupender Ram Niwas Ojha <b.ram.niwas.ojha@gmail.com>
2022-05-16 23:12:39 +0530

    Time: 415 ms (12.88%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 select name, population, area
 from world
-where population>=25000000 or area>=3000000;
+where population>=25000000 or area>=3000000
+order by name;
